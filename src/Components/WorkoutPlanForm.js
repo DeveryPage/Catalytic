40a7019395a572c6fdf1e66c5d6e98670df58214
Devery Page <pagedevery@gmail.com>
2021-10-20 14:47:05 -0500

    have all the form now and data entered into form posts to json

--- src/Components/WorkoutPlanForm.js
+++ src/Components/WorkoutPlanForm.js
@@ -3,58 +3,107 @@ Form : has all properties of workout plan : plan id sets reps etc.
 Need fetch that gets all workouts
 */
 import { getAllWorkouts } from './Plans/PlanManager';
-import React, { useState} from 'react';
-import { useHistory } from 'react-router-dom';
-import { addPlan } from './PlanManager';
+import React, { useState, useEffect} from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { addPlandWorkout } from './Plans/PlanManager';
+import { getAllPlandWorkouts } from './Plans/PlanManager';
 
 export const WorkoutPlanForm = () => {
     const history = useHistory();
-    const currentUser = JSON.parse(sessionStorage.getItem("catalytic_user"))
+    const {workoutPlanId} = useParams();
 
-    const [workoutPlan, setPlan] = useState ({
-        userId: parseInt(currentUser.id),
-        name: ""
-    });
+    const [workouts, setWorkouts] = useState ([]);
+
+    const [plandWorkout, setPlandWorkout] = useState({
+        workoutId: 0,
+        workoutPlanId: 0,
+        reps: 0,
+        restTime: ""
+    })
+
+    // useEffect(() => {
+    //     getAllPlandWorkouts().then((res) => {
+    //         setPlandWorkouts(res)
+    //     })
+    // }, [])
+
+    
+    useEffect(() => {
+        getAllWorkouts().then((res) => {
+            setWorkouts(res)
+        })
+    },[])
+
 
     const handleControlledInputChange = (event) => {
-        
-        const newPlan = {...workoutPlan }
+        const newWorkout = {...plandWorkout }
         let selectedVal = event.target.value
 
         if (event.target.id.includes("Id")) {
             selectedVal = parseInt(selectedVal)
         }
 
-        newPlan[event.target.id] = selectedVal
-        newPlan.userId = currentUser.id
-        setPlan(newPlan)
+        newWorkout[event.target.id] = selectedVal
+        setPlandWorkout(newWorkout)
     }
 
-    const handleClickSavePlan = (event) => {
+    const handleClickSaveWorkout = (event) => {
         event.preventDefault()
-        
-        if (workoutPlan.name === "") {
-            window.alert("Please choose a name")
-        } else {
-            addPlan(workoutPlan)
-                .then((res) => history.push(`/workoutPlans/${res.id}`))
+        const myNewPlan = {
+            workoutId: plandWorkout.workoutId,
+            workoutPlanId: parseInt(workoutPlanId),
+            sets: parseInt(plandWorkout.sets),
+            reps: parseInt(plandWorkout.reps),
+            restTime: plandWorkout.restTime
         }
-    }
+            addPlandWorkout(myNewPlan)
+                .then((res) => history.push(`/plandWorkouts/${res.id}`))
 
-    console.log(currentUser.id)
+    }
+    console.log(workouts)
     return (
-        <form className="planForm">
-            <h2 className="planform_title">New Workout Plan</h2>
+        <form className="workoutForm">
+            <h2 className="workoutform_title">New Workout</h2>
             <fieldset>
                 <div className="form-group">
-                    <label htmlFor="name">Plan Name: </label>
-                    <input type="text" id="name" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Plan Name" value={workoutPlan.name} />
+                    <label htmlFor="name">Workout: </label>
+                    <select type="select" onChange={handleControlledInputChange} id="workoutId" required autoFocus className="form-control">
+                        <option value="0" >Please Select A Workout...</option>
+                        {workouts.map(workout => <option key={workout.id} value={workout.id}>{workout.name}</option>)}
+                    </select>
+                    <select type="select" onChange={handleControlledInputChange} id="workoutId" required autoFocus className="form-control">
+                        <option value="0" >Please Select A Workout...</option>
+                        {workouts.map(workout => <option key={workout.id} value={workout.id}>{workout.name}</option>)}
+                    </select>
+                    <select type="select" onChange={handleControlledInputChange} id="workoutId" required autoFocus className="form-control">
+                        <option value="0" >Please Select A Workout...</option>
+                        {workouts.map(workout => <option key={workout.id} value={workout.id}>{workout.name}</option>)}
+                    </select>
+                    
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="sets">Sets </label>
+                    <input type="select" id="sets" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Sets" value={workouts.sets} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="reps">Reps </label>
+                    <input type="select" id="reps" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Repetitions" value={workouts.reps} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="restTime">Rest Time </label>
+                    <input type="select" id="restTime" onChange={handleControlledInputChange} required autoFocus className="form-control" placeholder="Rest Time" value={workouts.restTime} />
                 </div>
             </fieldset>
             <button 
-            className="savePlanName_btn"
-            onClick={handleClickSavePlan}>
-                Save Plan Name
+            className="saveWorkout_btn"
+            onClick={handleClickSaveWorkout}>
+                Save Workout
             </button>
         </form>
     )
